c627e2f56f5656a2b1360933ef4bfeef950f74b7
cjimenez23miner <cjimenez23@miners.utep.edu>
2017-10-28 22:31:11 -0600

    Add files via upload

--- app/contracts/compile.sol
+++ app/contracts/compile.sol
@@ -1,93 +1,29 @@
 pragma solidity ^0.4.0;
 
-interface ERC20 {
-    function transferFrom(address _from, address _to, uint _value) public returns (bool);
-    function approve(address _spender, uint _value) public returns (bool);
-    function allowance(address _owner, address _spender) public constant returns (uint);
-    event Approval(address indexed _owner, address indexed _spender, uint _value);
-}
+contract MyFirstToken{ // is Token("SC", "SmartChain", 18, 1000){//, ERC20, ERC223 {
 
-interface ERC223 {
-    function transfer(address _to, uint _value, bytes _data) public returns (bool);
-    event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
-}
-
-contract ERC223ReceivingContract {
-    function tokenFallback(address _from, uint _value, bytes _data) public;
-}
-
-contract Token {
     string internal _symbol;
     string internal _name;
     uint8 internal _decimals;
-    uint internal _totalSupply = 1000;
+//    uint internal _totalSupply;
     mapping (address => uint) internal _balanceOf;
     mapping (address => mapping (address => uint)) internal _allowances;
 
-    function Token(string symbol, string name, uint8 decimals, uint totalSupply) public {
+    event Approval(address indexed _owner, address indexed _spender, uint _value);
+    event Transfer(address indexed _from, address indexed _to, uint _value);
+    event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
+
+    function MyFirstToken(string symbol, string name, uint8 decimals, uint totalSupply) public {
         _symbol = symbol;
         _name = name;
         _decimals = decimals;
-        _totalSupply = totalSupply;
+        _balanceOf[msg.sender] = totalSupply;
     }
 
-    function name() public constant returns (string) {
-        return _name;
-    }
-
-    function symbol() public constant returns (string) {
-        return _symbol;
-    }
-
-    function decimals() public constant returns (uint8) {
-        return _decimals;
-    }
+ //   function MyFirstToken() public {     _balanceOf[msg.sender] = _totalSupply;}
 
     function totalSupply() public constant returns (uint) {
-        return _totalSupply;
-    }
-
-    function balanceOf(address _addr) public constant returns (uint);
-    function transfer(address _to, uint _value) public returns (bool);
-    event Transfer(address indexed _from, address indexed _to, uint _value);
-}
-
-library SafeMath {
-  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
-
-  function div(uint256 a, uint256 b) internal constant returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
-
-  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  function add(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
-}
-
-contract MyFirstToken is Token("MFT", "My First Token", 18, 1000), ERC20, ERC223 {
-
-    using SafeMath for uint;
-
-    function MyFirstToken() public {
-        _balanceOf[msg.sender] = _totalSupply;
-    }
-
-    function totalSupply() public constant returns (uint) {
-        return _totalSupply;
+        return _balanceOf[msg.sender];
     }
 
     function balanceOf(address _addr) public constant returns (uint) {
@@ -98,8 +34,8 @@ contract MyFirstToken is Token("MFT", "My First Token", 18, 1000), ERC20, ERC223
         if (_value > 0 &&
             _value <= _balanceOf[msg.sender] &&
             !isContract(_to)) {
-            _balanceOf[msg.sender] = _balanceOf[msg.sender].sub(_value);
-            _balanceOf[_to] = _balanceOf[_to].add(_value);
+            _balanceOf[msg.sender] -= _value;
+            _balanceOf[_to] += _value;
             Transfer(msg.sender, _to, _value);
             return true;
         }
@@ -110,10 +46,10 @@ contract MyFirstToken is Token("MFT", "My First Token", 18, 1000), ERC20, ERC223
         if (_value > 0 &&
             _value <= _balanceOf[msg.sender] &&
             isContract(_to)) {
-            _balanceOf[msg.sender] = _balanceOf[msg.sender].sub(_value);
-            _balanceOf[_to] = _balanceOf[_to].add(_value);
-            ERC223ReceivingContract _contract = ERC223ReceivingContract(_to);
-            _contract.tokenFallback(msg.sender, _value, _data);
+            _balanceOf[msg.sender] -= _value;
+            _balanceOf[_to] += _value;
+
+            
             Transfer(msg.sender, _to, _value, _data);
             return true;
         }
@@ -133,9 +69,9 @@ contract MyFirstToken is Token("MFT", "My First Token", 18, 1000), ERC20, ERC223
             _value > 0 &&
             _allowances[_from][msg.sender] >= _value &&
             _balanceOf[_from] >= _value) {
-            _balanceOf[_from] = _balanceOf[_from].sub(_value);
-            _balanceOf[_to] = _balanceOf[_to].add(_value);
-            _allowances[_from][msg.sender] = _allowances[_from][msg.sender].sub(_value);
+            _balanceOf[_from] -= _value;
+            _balanceOf[_to] += _value;
+            _allowances[_from][msg.sender] -= _value;
             Transfer(_from, _to, _value);
             return true;
         }
@@ -143,7 +79,7 @@ contract MyFirstToken is Token("MFT", "My First Token", 18, 1000), ERC20, ERC223
     }
 
     function approve(address _spender, uint _value) public returns (bool) {
-        _allowances[msg.sender][_spender] = _allowances[msg.sender][_spender].add(_value);
+        _allowances[msg.sender][_spender] += _value;
         Approval(msg.sender, _spender, _value);
         return true;
     }
